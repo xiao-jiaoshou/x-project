f40f8b1fc52b6340d4dcba7717115c4251c31506
11011306 <xiaokang06@yhwins.com>
2024-03-08 18:04:06 +0800

    2

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from 'vue'
+
+const count = ref(0)
+function increment(){
+  count.value++
+}
+
+</script>
+
+<template>
+  <main>
+    <button @click="increment">
+      {{count}}
+    </button>
+    <TheWelcome />
+  </main>
+</template>
